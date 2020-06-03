115eb9c37f631eb813913a887e89c1341eb8fe50
dachafra <dachafra@gmail.com>
2020-06-03 18:55:48 +0200

    download script

--- CIKM/data/download.sh
+++ CIKM/data/download.sh
@@ -2,7 +2,9 @@
 
 mkdir simple
 mkdir complex
-wget -O simple/data.zip https://delicias.dia.fi.upm.es/nextcloud/index.php/s/BBFFYYRJ3Ag9QDn/download
-unzip simple/data.zip
-wget -O complex/data-source10.zip https://delicias.dia.fi.upm.es/nextcloud/index.php/s/EGyr8EGAYwKqmD8/download
-unzip complex/data-source10.zip
+cd simple
+wget -O data.zip https://delicias.dia.fi.upm.es/nextcloud/index.php/s/BBFFYYRJ3Ag9QDn/download
+unzip data.zip
+cd ../complex
+wget -O data-source10.zip https://delicias.dia.fi.upm.es/nextcloud/index.php/s/EGyr8EGAYwKqmD8/download
+unzip data-source10.zip
